c9d1d60f62a12389e9bf963dbf2c96ebc1c693b2
quynhnv2202 <128356107+quynhnv2202@users.noreply.github.com>
2023-06-01 22:35:19 +0700

    Change path HomePage

---
+++ AssignmentPRJ301Su23/src/java/Context/DBContext.java
@@ -0,0 +1,28 @@
+package Context;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class DBContext {
+    
+    /*USE BELOW METHOD FOR YOUR DATABASE CONNECTION FOR BOTH SINGLE AND MULTILPE SQL SERVER INSTANCE(s)*/
+ /*DO NOT EDIT THE BELOW METHOD, YOU MUST USE ONLY THIS ONE FOR YOUR DATABASE CONNECTION*/
+    public Connection getConnection() throws Exception {
+        String url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+        if (instance == null || instance.trim().isEmpty()) {
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        }
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(url, userID, password);
+    }
+    private final String serverName = "DESKTOP-E1MJCMF";
+    private final String dbName = "AssignmentPRJ301Su23";
+    private final String portNumber = "1433";
+    private final String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final String userID = "quynhnvhe163568";
+    private final String password = "123";
+  
+}
